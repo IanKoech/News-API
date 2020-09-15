060fd8162acf95c89628315b83e302b90c04de08
Ian K <iankoech9@gmail.com>
2020-09-15 14:35:03 +0300

    Added html files inside templates directory

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Articles:
+    '''
+    Class to define article objects
+    '''
+    def __init__(self,title,author,description,date_published,url):
+        self.title=title
+        self.author=author
+        self.description=description
+        self.date_published=date_published
+        self.url=url
