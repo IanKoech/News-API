e6a7b06d60972e2694eb757511ca69bbf77a5db8
Ian K <iankoech9@gmail.com>
2020-09-15 14:12:18 +0300

    Recreted the file

--- app/models.py
+++
@@ -1,8 +0,0 @@
-class Articles:
-    def __init__(self,author,title,date_published,image_url,description,url):
-        self.author=author
-        self.title=title
-        self.date_published=date_published
-        self.image_url=image_url
-        self.description=description
-        self.url=url
--- app/requests.py
+++
@@ -1,8 +0,0 @@
-import urllib.request,json
-from .models import Articles
-#Getting the api key
-api_key=None
-#Getting movie base url
-base_url=None
-Articles=article.Articles
-#Created instance object of Articles class
